4916e2f16e062d4610ed894344b141d1fc7a42c8
kevinklev <kevinkm2009@gmail.com>
2023-09-10 13:13:51 -0400

    Added functions

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+
+
 class Hotel:
-    def view_hotels(self):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
 
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking details:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+
+print(df)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not available")
